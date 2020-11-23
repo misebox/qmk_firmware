813610debd5026a46cd352789da70392e4488aaa
misebox <misebox@gmail.com>
2020-11-23 19:50:49 +0900

    migrate to 4x12

--- keyboards/planck/keymaps/misebox/keymap.c
+++ keyboards/planck/keymaps/misebox/keymap.c
@@ -8,10 +8,10 @@
 
 #include "muse.h"
 
-enum planck_layers { _QWERTY, _FUNCFULL, _MOVE, _SYMBOL, _TENKEY, _SPECIAL };
+enum planck_layers { _QWERTY, _FUNCFULL, _MOVE, _SYMBOL, _TENKEY, _SPECIAL, _KEYBOARD };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_QWERTY] = LAYOUT_planck_1x2uC(
+	[_QWERTY] = LAYOUT_planck_grid(
 // Qwerty
 //  ,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS,
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
 //  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
-     MO(4),KC_LGUI,KC_LALT,LT(3,KC_LANG2),LT(1,KC_TAB),LSFT_T(KC_SPC),LT(2,KC_ENT),KC_RCTL,KC_RALT,KC_APP,KC_LANG1),
+     LT(4,KC_HOME),KC_LGUI,LALT_T(KC_DEL),LT(3,KC_LANG2),MO(1),LSFT_T(KC_SPC),KC_LSFT,LT(2,KC_ENT),RCTL_T(KC_TAB),RALT_T(KC_LANG1),KC_APP,LT(5,KC_END)),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 
-	[_FUNCFULL] = LAYOUT_planck_1x2uC(
+	[_FUNCFULL] = LAYOUT_planck_grid(
 // Functions and Full key
 //  ,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
       KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
@@ -32,10 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11, KC_F12,
 //  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,   MO(5), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 
-	[_MOVE] = LAYOUT_planck_1x2uC(
+	[_MOVE] = LAYOUT_planck_grid(
 // Cursor and page transition
 //  ,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
        KC_ESC,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,KC_PGUP,RSFT(KC_TAB),KC_TAB, KC_PGDN, KC_RCTL, KC_BSPC,
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_DEL, KC_HOME,  KC_END, KC_BSPC, KC_RSFT,   KC_NO,
 //  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   MO(5),          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 
 // Structural symbols
-	[_SYMBOL] = LAYOUT_planck_1x2uC(
+	[_SYMBOL] = LAYOUT_planck_grid(
 //  ,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
        KC_ESC,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_GRV, KC_QUOT, KC_DQUO,   KC_NO, KC_BSPC,
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
@@ -56,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_LPRN,   KC_LT,   KC_GT, KC_RPRN,   KC_NO,
 //  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 
 // Ten key
-	[_TENKEY] = LAYOUT_planck_1x2uC(
+	[_TENKEY] = LAYOUT_planck_grid(
 //  ,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
       KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_PSLS,   KC_P7,   KC_P8,   KC_P9, KC_PAST, KC_BSPC,
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
@@ -68,19 +68,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NLCK,   KC_P1,   KC_P2,   KC_P3, KC_PEQL,   KC_NO,
 //  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,   KC_P0, KC_PCMM, KC_PDOT, KC_PENT,   KC_NO),
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_P0, KC_PCMM, KC_PDOT, KC_PENT, MO(6)),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 
-	[_SPECIAL] = LAYOUT_planck_1x2uC(
+	[_SPECIAL] = LAYOUT_planck_grid(
 // Special controls
 //  ,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
-        RESET,   DEBUG,   KC_NO, KC_BRID, KC_BRIU,   KC_NO, KC_MUTE, KC_VOLD, KC_VOLU,   KC_NO,   KC_NO,  MU_OFF,
+        KC_NO, RGB_SAD, RGB_HUD, RGB_HUI, RGB_SAI, RGB_TOG,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-      KC_CAPS, BL_TOGG, BL_BRTG,RGB_RMOD, RGB_MOD, RGB_TOG, RGB_SAD, RGB_HUD, RGB_HUI, RGB_SAI,  MU_MOD, KC_NLCK,
+     RGB_RMOD, RGB_SPD, RGB_VAD, RGB_VAI, RGB_SPI, RGB_MOD,   KC_NO,   KC_NO, KC_BRID, KC_BRIU,   KC_NO,   KC_NO,
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-      KC_TRNS, BL_STEP,  BL_OFF,  BL_DEC,  BL_INC,   BL_ON, RGB_SPD, RGB_VAD, RGB_VAI, RGB_SPI,  MUV_DE,  MUV_IN,
+      KC_TRNS, BL_STEP,  BL_OFF,  BL_DEC,  BL_INC,   BL_ON,   KC_NO, KC_MUTE, KC_VOLD, KC_VOLU,   KC_NO,   KC_NO,
 //  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
-      LCG_SWP, LCG_NRM,   KC_NO,   KC_NO, KC_TRNS,          RGB_SAD, KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   MU_ON)
+        MO(6),   KC_NO,   KC_NO, BL_TOGG, BL_BRTG,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS),
+//  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
+// LCG_SWP, LCG_NRM
+
+	[_KEYBOARD] = LAYOUT_planck_grid(
+// Keyboard reset and music mode
+//  ,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
+        RESET,   DEBUG,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+//  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
+      KC_CAPS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NLCK,
+//  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+//  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
+      KC_TRNS,   KC_NO,   KC_NO,  MU_MOD,  MUV_DE,  MU_OFF,   MU_ON,  MUV_IN,  MU_MOD,   KC_NO,   KC_NO, KC_TRNS)
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 };
 
