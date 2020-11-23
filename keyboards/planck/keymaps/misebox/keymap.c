da7a5393e279a5604033c269a890c520e7ae9067
misebox <misebox@gmail.com>
2020-11-23 22:44:18 +0900

    Improvement row1

--- keyboards/planck/keymaps/misebox/keymap.c
+++ keyboards/planck/keymaps/misebox/keymap.c
@@ -8,6 +8,7 @@
 
 #include "muse.h"
 
+
 enum planck_layers { _QWERTY, _FUNCFULL, _MOVE, _SYMBOL, _TENKEY, _SPECIAL, _KEYBOARD };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -19,8 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
-//  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
-     LT(4,KC_HOME),KC_LGUI,LALT_T(KC_DEL),LT(3,KC_LANG2),MO(1),LSFT_T(KC_SPC),KC_LSFT,LT(2,KC_ENT),RCTL_T(KC_TAB),RALT_T(KC_LANG1),KC_APP,LT(5,KC_END)),
+//  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
+//  |L4/ Home|  LGUI  |  LALT  |L3/ Eisu|L1 / TAB|LSFT/SPC|RSFT/ BS|L2/ENTER|CTL/Kana|  RALT  |  RGUI  |L5 / END|
+     LT(4,KC_HOME),KC_LGUI,KC_LALT,LT(3,KC_LANG2),LT(1,KC_TAB),LSFT_T(KC_SPC),RSFT_T(KC_BSPC),LT(2,KC_ENT),RCTL_T(KC_LANG1),KC_RALT,KC_RGUI,LT(5,KC_END)),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 
 	[_FUNCFULL] = LAYOUT_planck_grid(
@@ -31,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11, KC_F12,
-//  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
+//  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 
@@ -43,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_ESC, KC_RALT,
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_DEL, KC_HOME,  KC_END, KC_BSPC, KC_RSFT,   KC_NO,
-//  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
+//  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 
@@ -55,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC,   KC_NO,
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_LPRN,   KC_LT,   KC_GT, KC_RPRN,   KC_NO,
-//  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
+//  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 
@@ -67,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_PMNS,   KC_P4,   KC_P5,   KC_P6, KC_PPLS,   KC_NO,
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NLCK,   KC_P1,   KC_P2,   KC_P3, KC_PEQL,   KC_NO,
-//  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
+//  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_P0, KC_PCMM, KC_PDOT, KC_PENT, MO(6)),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 
@@ -79,10 +81,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      RGB_RMOD, RGB_SPD, RGB_VAD, RGB_VAI, RGB_SPI, RGB_MOD,   KC_NO,   KC_NO, KC_BRID, KC_BRIU,   KC_NO,   KC_NO,
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
       KC_TRNS, BL_STEP,  BL_OFF,  BL_DEC,  BL_INC,   BL_ON,   KC_NO, KC_MUTE, KC_VOLD, KC_VOLU,   KC_NO,   KC_NO,
-//  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
+//  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
         MO(6),   KC_NO,   KC_NO, BL_TOGG, BL_BRTG,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS),
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
-// LCG_SWP, LCG_NRM
+//
 
 	[_KEYBOARD] = LAYOUT_planck_grid(
 // Keyboard reset and music mode
@@ -93,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 //  |--------|--------|--------|--------|--------|-----------------|--------|--------|--------|--------|--------|
-      KC_TRNS,   KC_NO,   KC_NO,  MU_MOD,  MUV_DE,  MU_OFF,   MU_ON,  MUV_IN,  MU_MOD,   KC_NO,   KC_NO, KC_TRNS)
+      KC_TRNS,   KC_NO, LCG_SWP,  MU_MOD,  MUV_DE,  MU_OFF,   MU_ON,  MUV_IN,  MU_MOD, LCG_NRM,   KC_NO, KC_TRNS)
 //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
 };
 
