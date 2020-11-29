5e58adede106cd644a62948012dbbe69ec03bfe6
misebox <misebox@gmail.com>
2020-11-29 13:57:56 +0900

    Move function keys from L1 to L4

--- keyboards/planck/keymaps/misebox/keymap.c
+++ keyboards/planck/keymaps/misebox/keymap.c
@@ -2,7 +2,7 @@
 
 #include "muse.h"
 
-enum planck_layers { _QWERTY, _FUNCFULL, _MOVE, _SYMBOL, _TENKEY, _SPECIAL, _KEYBOARD };
+enum planck_layers { _QWERTY, _FULL, _MOVE, _SYMBOL, _TENFUNC, _SPECIAL, _KEYBOARD };
 
 enum custom_keycodes {  // Make sure have the awesome keycode ready
     L4_STAB = SAFE_RANGE,
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               L4_STAB, KC_LGUI, KC_LALT, CT_EISU,  L1_TAB,  ST_SPC, L3_BSPC,  L2_ENT, KC_RCTL, KC_RALT, KC_RGUI,  L5_TAB
         //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
     ),
-    [_FUNCFULL] = LAYOUT_planck_grid(
-        // Functions and Full key
+    [_FULL] = LAYOUT_planck_grid(
+        // Full key
         //  ,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
               KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
         //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
                KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_EQL,
         //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-                KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
+              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
               _______, _______, _______, _______, _______, _______, _______, _______,KC_LANG1, _______, _______, _______
         //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
@@ -73,14 +73,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
     ),
-    [_TENKEY] = LAYOUT_planck_grid(
-        // Ten key
+    [_TENFUNC] = LAYOUT_planck_grid(
+        // Ten key and functions
         //  ,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
-              _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS,   KC_P7,   KC_P8,   KC_P9, KC_PAST, KC_BSPC,
+              _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_PSLS,   KC_P7,   KC_P8,   KC_P9, KC_PAST, KC_BSPC,
         //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-              _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PMNS,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, XXXXXXX,
+              _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_PMNS,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, XXXXXXX,
         //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-              _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NLCK,   KC_P1,   KC_P2,   KC_P3, KC_PEQL, XXXXXXX,
+              _______,  KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15, KC_NLCK,   KC_P1,   KC_P2,   KC_P3, KC_PEQL, XXXXXXX,
         //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
               _______, _______, _______, _______, _______, _______, _______,   KC_P0, KC_COMM, KC_PDOT, KC_PENT,   MO(6)
         //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
@@ -121,9 +121,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case L4_STAB: {
             if (record->event.pressed) {
-                layer_on(_TENKEY);
+                layer_on(_TENFUNC);
             } else {
-                layer_off(_TENKEY);
+                layer_off(_TENFUNC);
                 if (is_tapped) {
                     register_code(KC_LSFT);
                     register_code(KC_TAB);
