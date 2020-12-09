03709dd9e45f5c80989798557741ec7b047f9988
misebox <misebox@gmail.com>
2020-12-09 23:41:55 +0900

    Swap right shift and ESC_Eisu on MOVE layer

--- keyboards/planck/keymaps/misebox/keymap.c
+++ keyboards/planck/keymaps/misebox/keymap.c
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //  ,--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------.
                KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP,   S_TAB,  KC_TAB, KC_PGDN, KC_RCTL, KC_BSPC,
         //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-              _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,ESC_EISU, KC_RALT,
+              _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_RSFT, KC_RALT,
         //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
-              _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_DEL, KC_HOME,  KC_END, KC_BSPC, KC_RSFT, XXXXXXX,
+              _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_DEL, KC_HOME,  KC_END, KC_BSPC,ESC_EISU, XXXXXXX,
         //  |--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|--------|
               _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         //  `--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------'
